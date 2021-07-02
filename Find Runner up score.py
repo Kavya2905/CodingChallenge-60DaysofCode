99ea2195118f3e25cf78a309c5bb9ceaabc055d3
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-07-02 23:16:16 +0530

    Update Find Runner up score.py

--- Find Runner up score.py
+++ Find Runner up score.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     res = [int(x) for x in arr]
-    k = list(set(res))
+    k = list(set(res)) #to remove duplicates
     k.remove(max(k))
     print(max(k))
