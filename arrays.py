8f84c72be9a4fc17b073acf9d46746748ace1fbc
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-07-15 16:20:07 +0530

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    s = arr[::-1]
+    for i in s:
+        print(i, end=' ')
