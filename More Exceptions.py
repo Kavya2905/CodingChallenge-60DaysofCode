eaaef3a0c68388da32f5f525a42ebeb1f1ff6af4
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-07-25 13:04:53 +0530

    Create More Exceptions.py

---
+++ More Exceptions.py
@@ -0,0 +1,21 @@
+#Write your code here
+class Calculator:
+    def power(self, n, p):
+        
+        
+        if n|p < 0:
+            raise Exception("n and p should be non-negative")
+        else:
+            return n**p
+
+        
+    
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
