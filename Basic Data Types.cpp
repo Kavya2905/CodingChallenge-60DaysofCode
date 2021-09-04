993e1288a554e5f1d4dbd7f163b0fb16534b7dc4
Kavya2905 <74311514+Kavya2905@users.noreply.github.com>
2021-09-04 18:25:47 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int k;
+    long l;
+    char m;
+    float n; 
+    double o;
+    cin>>k>>l>>m>>n>>o;
+    printf("%d\n%ld\n%c\n%f\n%lf\n", k, l, m, n, o);
+    return 0;
+}
